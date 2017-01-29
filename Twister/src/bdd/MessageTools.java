0c47912285d30c9093bfd9db8ca95324e862524c
Kayrin <hvu295@gmail.com>
2017-01-29 22:06:58 +0100

    update 290117

---
+++ Twister/src/bdd/MessageTools.java
@@ -0,0 +1,24 @@
+package bdd;
+
+import org.json.JSONObject;
+import java.sql.SQLException;
+
+import services.ErrorJSON;
+import services.ServicesTools;
+
+public class MessageTools {
+	
+	public JSONObject createMessage(String user, String message){
+		JSONObject ret = new JSONObject();
+		
+		/* générer message_id dans BDD */
+		
+		return ret;
+	}
+	
+	public JSONObject listMessages(String user){
+		JSONObject ret = new JSONObject();
+		
+		return ret;
+	}
+}
